b41d7a2add6dde0469bbb9c8ba661f878b193aa9
Koukouyi710 <yangmh@qq.com>
2019-08-07 15:32:56 +0800

    添加拦截器

--- src/main/java/com/neuedu/interceptors/AuthorityInterceptor.java
+++ src/main/java/com/neuedu/interceptors/AuthorityInterceptor.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Component
 public class AuthorityInterceptor implements HandlerInterceptor{
@@ -14,7 +16,18 @@ public class AuthorityInterceptor implements HandlerInterceptor{
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
         System.out.println("=====preHandle======");
-        return true;//false:拦截请求 true:不会拦截请求
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user")!=null){
+            return true;
+        }
+        else {
+            try {
+            response.sendRedirect(request.getContextPath()+"/user/login");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;//false:拦截请求 true:不会拦截请求
+        }
     }
 
     @Override
